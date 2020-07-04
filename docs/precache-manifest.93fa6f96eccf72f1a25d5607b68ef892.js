19991124f2357e12a5e4e397c2632354f11d1eb6
praveensrinivas <travancorepraveen@gmail.com>
2020-07-04 17:28:23 +0530

    Added build files to doc folder to host in github pages

---
+++ docs/precache-manifest.93fa6f96eccf72f1a25d5607b68ef892.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: 'e545463b97bf63deb5c3b90762185cce',
+    url: '/index.html',
+  },
+  {
+    revision: '082dd0c907ee3d41f83d',
+    url: '/static/js/bundle.js',
+  },
+]);
